279ba55a4f95741ee59d867e4f74fcc83cb05987
HaroldDev <tinaritiziano.dev@gmail.com>
2022-05-25 09:13:45 +0200

    Add methods in Gioco class

--- MyLibrary/src/main/java/com/mylibrary/service/GiocoService.java
+++ MyLibrary/src/main/java/com/mylibrary/service/GiocoService.java
@@ -8,6 +8,7 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.mylibrary.model.Film;
 import com.mylibrary.model.Gioco;
 import com.mylibrary.repository.GiocoRepository;
 
@@ -16,29 +17,29 @@ public class GiocoService {
 	
 	@Autowired
 	private GiocoRepository giocoRepository;
-	
+
 	@Transactional
 	public void save(Gioco gioco) {
 		this.giocoRepository.save(gioco);
 	}
-	
+
 	public Gioco findGiocoById(Long id) {
 		return this.giocoRepository.findById(id).get();
 	}
-	
+
 	public List<Gioco> findAllGiochi() {
 		List<Gioco> giochi = new ArrayList<>();
-		
+
 		for(Gioco gioco : this.giocoRepository.findAll()) {
 			giochi.add(gioco);
 		}
-		
+
 		return giochi;
 	}
-	
+
 	@Transactional
 	public void deleteGioco(Gioco gioco) {
 		this.giocoRepository.delete(gioco);
 	}
-	
+
 }
