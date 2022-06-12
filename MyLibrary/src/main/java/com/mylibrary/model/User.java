726acb29b2ff0aa277b3455f91538997a641c1b9
oleksandrpoddubnyy <oleksandr.poddu@gmail.com>
2022-06-12 22:39:15 +0200

    home page

--- MyLibrary/src/main/java/com/mylibrary/model/User.java
+++ MyLibrary/src/main/java/com/mylibrary/model/User.java
@@ -7,37 +7,38 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "users") // user in postgres è una parola riservata
+@Table(name = "users") // cambiamo nome perchè in postgres user e' una parola riservata
 public class User {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String nome;
 	private String cognome;
-	
+
 	public Long getId() {
 		return id;
 	}
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public String getCognome() {
 		return cognome;
 	}
-	
+
 	public void setCognome(String cognome) {
 		this.cognome = cognome;
 	}
 
 }
+
--- MyLibrary/src/main/java/com/mylibrary/service/CredentialsService.java
+++ MyLibrary/src/main/java/com/mylibrary/service/CredentialsService.java
@@ -12,13 +12,13 @@ import com.mylibrary.repository.CredentialsRepository;
 
 @Service
 public class CredentialsService {
-	
-    @Autowired
-    protected PasswordEncoder passwordEncoder;
+
+	@Autowired
+	protected PasswordEncoder passwordEncoder;
 
 	@Autowired
 	protected CredentialsRepository credentialsRepository;
-	
+
 	@Transactional
 	public Credentials getCredentials(Long id) {
 		Optional<Credentials> result = this.credentialsRepository.findById(id);
@@ -30,11 +30,11 @@ public class CredentialsService {
 		Optional<Credentials> result = this.credentialsRepository.findByUsername(username);
 		return result.orElse(null);
 	}
-		
-    @Transactional
-    public Credentials saveCredentials(Credentials credentials) {
-        credentials.setRole(Credentials.DEFAULT_ROLE);
-        credentials.setPassword(this.passwordEncoder.encode(credentials.getPassword()));
-        return this.credentialsRepository.save(credentials);
-    }
+
+	@Transactional
+	public Credentials saveCredentials(Credentials credentials) {
+		credentials.setRole(Credentials.DEFAULT_ROLE);
+		credentials.setPassword(this.passwordEncoder.encode(credentials.getPassword()));
+		return this.credentialsRepository.save(credentials);
+	}
 }
