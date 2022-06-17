51f050a703126a3fcbad01621d2c04faeb5960cf
oleksandrpoddubnyy <oleksandr.poddu@gmail.com>
2022-06-17 17:51:26 +0200

    home page

--- MyLibrary/target/classes/static/js/login.js
+++ MyLibrary/target/classes/static/js/login.js
@@ -1,17 +1,54 @@
-const pwShowHide = document.querySelector(".showHidePw"),
-    pwFields = document.querySelector(".password");
+const containerlog = document.querySelector(".login"),
+    containersing = document.querySelector(".signup"),
+    pwShowHide = document.querySelectorAll(".showHidePw"),
+    pwFields = document.querySelectorAll(".password"),
+    signUp = document.querySelector(".signup-link"),
+    blur = document.querySelector(".sfondo"),
+    pulsante = document.querySelector("#login-page"),
+    chiudiform = document.querySelectorAll(".close-bar"),
+    login = document.querySelector(".login-link");
 
 //   js code to show/hide password and change icon
-const showHidePW = () => {
-    if(pwFields.type ==="password"){
-        pwFields.type = "text";
+pwShowHide.forEach(eyeIcon =>{
+    eyeIcon.addEventListener("click", ()=>{
+        pwFields.forEach(pwField =>{
+            if(pwField.type ==="password"){
+                pwField.type = "text";
 
-        pwShowHide.classList.replace("uil-eye-slash", "uil-eye");
-    }else{
-        pwFields.type = "password";
+                pwShowHide.forEach(icon =>{
+                    icon.classList.replace("uil-eye-slash", "uil-eye");
+                })
+            }else{
+                pwField.type = "password";
 
-        pwShowHide.classList.replace("uil-eye", "uil-eye-slash");
-        }
-};
+                pwShowHide.forEach(icon =>{
+                    icon.classList.replace("uil-eye", "uil-eye-slash");
+                })
+            }
+        })
+    })
+})
 
-pwShowHide.addEventListener("click", showHidePW);
+pulsante.addEventListener("click", ( )=>{
+    containerlog.classList.add("active");
+    pulsante.classList.add("blur");
+})
+
+chiudiform.forEach(closeIcon =>{
+    closeIcon.addEventListener("click", ( )=>{
+        containerlog.classList.remove("active");
+        containersing.classList.remove("active");
+        pulsante.classList.remove("blur");
+    })
+})
+
+// js code to appear signup and login form
+signUp.addEventListener("click", ( )=>{
+    containersing.classList.add("active");
+    containerlog.classList.remove("active");
+})
+
+login.addEventListener("click", ( )=>{
+    containerlog.classList.add("active");
+    containersing.classList.remove("active");
+})
