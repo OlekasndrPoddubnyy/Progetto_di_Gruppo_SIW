7f26be6b68442f322dcd3c8e87c4213421fd4679
oleksandrpoddubnyy <oleksandr.poddu@gmail.com>
2022-06-29 15:33:29 +0200

    home page

--- MyLibrary/src/main/java/com/mylibrary/controller/MainController.java
+++ MyLibrary/src/main/java/com/mylibrary/controller/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 @Controller
 public class MainController {
 
@@ -12,4 +13,5 @@ public class MainController {
 	public String index(Model model) {
 		return "index";
 	}
+
 }
