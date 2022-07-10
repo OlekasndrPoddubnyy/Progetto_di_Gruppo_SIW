4dec22c51139944cbf2f0580d395a4e94ee077b4
oleksandrpoddubnyy <oleksandr.poddu@gmail.com>
2022-07-10 09:37:43 +0200

    dovrebbe funzionare tutto

--- MyLibrary/src/main/java/com/mylibrary/controller/AuthenticationController.java
+++ MyLibrary/src/main/java/com/mylibrary/controller/AuthenticationController.java
@@ -40,6 +40,9 @@ public class AuthenticationController {
 	private LibroService libroService;
 
 	@Autowired
+	private  UserService userService;
+
+	@Autowired
 	private GiocoService giocoService;
 
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
@@ -73,12 +76,13 @@ public class AuthenticationController {
 			return "admin/home";
 		}
 
-		User user = credentials.getUser();
-		model.addAttribute("filmPreferiti", user.getFilmPreferiti());
-		model.addAttribute("giochiPreferiti", user.getGiochiPreferiti());
-		model.addAttribute("serieTvPreferite", user.getSerieTvPreferite());
-		model.addAttribute("libriPreferiti", user.getLibriPreferiti());
-		return "home";
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (!(authentication instanceof AnonymousAuthenticationToken)) {
+			String currentUserName = authentication.getName();
+			Credentials credentials1 = this.credentialsService.getCredentials(currentUserName);
+			model.addAttribute("user", this.userService.getUser(credentials1.getUser().getId()));
+		}
+		return "/home";
 	}
 
 	@RequestMapping(value = { "/register" }, method = RequestMethod.POST)
